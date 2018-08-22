c287bff82f35f42bcad127d9cb2d595d7ca23fea
Paul Rocca <Paul@the-rocca-family.net>
2018-08-23 00:43:04 +0100

    add fly behaviour

--- src/main/java/duckclasses/DuckMain.java
+++ src/main/java/duckclasses/DuckMain.java
@@ -5,6 +5,8 @@ public class DuckMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Duck mallard = new MallardDuck();
+		mallard.performFly();
+		
 	}
 
 }
--- src/main/java/duckclasses/MallardDuck.java
+++ src/main/java/duckclasses/MallardDuck.java
@@ -3,6 +3,10 @@ package duckclasses;
 public class MallardDuck extends Duck{
 
 	
+	public MallardDuck() {
+		this.flyBehaviour = new FlyWithWings();
+	}
+
 	public void display() {
 		System.out.println("I'm a real mallard duck");
 		
